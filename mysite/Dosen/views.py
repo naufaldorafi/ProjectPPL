6fd0177cde2d4c98bbad69dd55cf8dcd5325e378
Zhillan <zhillan.arrafi@gmail.com>
2024-09-16 11:57:22 +0700

    push project awal

---
+++ mysite/Dosen/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def Dosen(request):
+    return render(request, 'dosen.html')
---
+++ mysite/idePenelitian/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def idePenelitian(request):
+    return render(request, 'idePenelitian.html')
