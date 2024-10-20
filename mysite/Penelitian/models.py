4ecf40a357a309ae2ebef47d1ccd557b0ebe861d
Zhillan <zhillan.arrafi@gmail.com>
2024-10-20 18:01:03 +0700

    create superuser dan setup model

--- mysite/Penelitian/models.py
+++ mysite/Penelitian/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class PenelitianDosen(models.Model):
+    judul = models.CharField(max_length= 50)
+
+    def __str__(self):
+        return self.judul
