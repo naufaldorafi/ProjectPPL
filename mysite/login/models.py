527123d55c7362b9c38c14f6410b8f08a5c96d29
bellanaomiii <serafimbella04@gmail.com>
2024-10-21 20:33:16 +0700

    User Models

--- mysite/login/models.py
+++ mysite/login/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Akun(models.Model):
+    Username = models.CharField(max_length=25)
+    Password = models.CharField(max_length=25)
+    Email = models.CharField(max_length=25)
+
+    def __str__(self):
+        return f"{self.Username}, {self.Password}, {self.Email}"
+
+    
