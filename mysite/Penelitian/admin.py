8e85608c92f4e322b972a0b4ed0fdd8324dc4e01
triastrale <atria.nur.farradina-2022@fst.unair.ac.id>
2024-10-21 18:09:22 +0700

    Integrasi database Penelitian dosen dan Topik penelitian

--- mysite/Penelitian/admin.py
+++ mysite/Penelitian/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from Penelitian.models import PenelitianDosen
+from Penelitian.models import PenelitianDosen, TopikPenelitian
 # Register your models here.
 
 admin.site.register(PenelitianDosen)
+admin.site.register(TopikPenelitian)
--- mysite/Penelitian/models.py
+++ mysite/Penelitian/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.   
 class PenelitianDosen(models.Model):
-    judul = models.CharField(max_length= 50)
+    judul = models.CharField(max_length=100)
+    abstract = models.CharField(max_length=250,null=True)
+    tanggal_publikasi = models.DateField(null=True)
+    tipe_publikasi = models.CharField(max_length=1,null=True)
+    sumber = models.CharField(max_length=250,null=True)
 
     def __str__(self):
         return self.judul
+    
+class TopikPenelitian(models.Model):
+    nama = models.CharField(max_length=100)
+    penelitiandosen_id = models.ForeignKey(PenelitianDosen, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.nama
